a4dcfb398f0e8369e08caeaef94baf2b19c1c593
Phan Thi Mai <1612369@student.hcmus.edu.vn>
2019-11-06 16:42:22 +0700

    Hot fix

---
+++ client/build/precache-manifest.9ae4cfc9bf5199592ff9c4542080ce42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d74b55efed3673b24194a60449d14a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd6ba1ebcc452ff3e13c",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "e8c6de8d31859f6bb64e",
+    "url": "/static/css/main.660dda3f.chunk.css"
+  },
+  {
+    "revision": "bd6ba1ebcc452ff3e13c",
+    "url": "/static/js/2.065bc417.chunk.js"
+  },
+  {
+    "revision": "e8c6de8d31859f6bb64e",
+    "url": "/static/js/main.748497c3.chunk.js"
+  },
+  {
+    "revision": "716a26e6381a724394c8",
+    "url": "/static/js/runtime-main.26ec62ec.js"
+  }
+]);
